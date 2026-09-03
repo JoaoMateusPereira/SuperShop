05dde1f3514963466387bfd8d37d7dde34c680ed
JoaoMateusPereira <joao_pereira1933@outlook.pt>
2026-09-03 14:08:59 +0100

    Generic repository

---
+++ SuperShop/Data/Entities/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data.Entities
+{
+    public interface IEntity
+    {
+        int Id { get; }
+
+    }
+}

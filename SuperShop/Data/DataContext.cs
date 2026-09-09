e917e28ee32a0e252f195902d76570ecfd6396e6
JoaoMateusPereira <joao_pereira1933@outlook.pt>
2026-09-09 12:43:58 +0100

    Add users

--- SuperShop/Data/DataContext.cs
+++ SuperShop/Data/DataContext.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
 
 namespace SuperShop.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DbSet<Product> Products { get; set; }
 

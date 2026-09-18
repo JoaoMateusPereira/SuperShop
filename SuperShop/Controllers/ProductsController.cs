f2ce9120e7db73fef174d4bdccd281fbbfbef0fa
JoaoMateusPereira <joao_pereira1933@outlook.pt>
2026-09-18 09:46:51 +0100

    Register new users

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -79,8 +79,7 @@ namespace SuperShop.Controllers
 
                 var product = _converterHelper.ToProduct(model, path, true);
 
-                //TODO: Modificar para o user que tiver logado
-                product.User = await _userHelper.GetUserByEmailAsync("rafaasfs@gmail.com");
+                product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 await _productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -130,7 +129,7 @@ namespace SuperShop.Controllers
                     var product = _converterHelper.ToProduct(model, path, false);
 
                     //TODO: Modificar para o user que tiver logado
-                    product.User = await _userHelper.GetUserByEmailAsync("rafaasfs@gmail.com");
+                    product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                     await _productRepository.UpdateAsync(product);
                 }
                 catch (DbUpdateConcurrencyException)
@@ -150,6 +149,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)

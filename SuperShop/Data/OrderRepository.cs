0a8b9d1aff5380b0cae121f8e95ae1034eba14a8
JoaoMateusPereira <joao_pereira1933@outlook.pt>
2026-09-26 09:25:54 +0100

    Increase, decrease and delete detail temp item

--- SuperShop/Data/OrderRepository.cs
+++ SuperShop/Data/OrderRepository.cs
@@ -57,6 +57,18 @@ namespace SuperShop.Data
             await _context.SaveChangesAsync();
         }
 
+        public async Task DeleteDetailTempAsync(int id)
+        {
+            var orderDetailTemp = await _context.OrderDetailTemp.FindAsync(id);
+            if(orderDetailTemp == null)
+            {
+                return;
+            }
+
+            _context.OrderDetailTemp.Remove(orderDetailTemp);
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<IQueryable<OrderDetailTemp>> GetDetailTempsAsync(string userName)
         {
             var user = await _userHelper.GetUserByEmailAsync(userName);
@@ -102,7 +114,7 @@ namespace SuperShop.Data
             }
 
             orderDetailTemp.Quantity += quantity;
-            if (orderDetailTemp.Quantity <= 0)
+            if (orderDetailTemp.Quantity > 0)
             {
                 _context.OrderDetailTemp.Update(orderDetailTemp);
                 await _context.SaveChangesAsync();
